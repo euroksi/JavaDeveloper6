509ca569b2bfc491faa87845417b6acbc60dcba3
euroksi <153302244+euroksi@users.noreply.github.com>
2024-10-07 08:51:14 +0200

    JavaDeveloper6

---
+++ src/main/java/org/example/DatabaseQueryService.java
@@ -0,0 +1,54 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DatabaseQueryService {
+
+    public List<MaxProjectCountClient> findMaxProjectsClient() {
+        String sqlFilePath = "sql/find_max_projects_client.sql";
+        List<MaxProjectCountClient> result = new ArrayList<>();
+
+        try (Connection conn = Database.getInstance().getConnection();
+             Statement stmt = conn.createStatement()) {
+
+            String query = readSqlFile(sqlFilePath);
+            ResultSet rs = stmt.executeQuery(query);
+
+            while (rs.next()) {
+                String name = rs.getString("name");
+                int projectCount = rs.getInt("project_count");
+                result.add(new MaxProjectCountClient(name, projectCount));
+            }
+        } catch (SQLException e) {
+            System.out.println("Помилка під час виконання запиту: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("Помилка при читанні SQL файлу: " + e.getMessage());
+        }
+
+        return result;
+    }
+
+    private String readSqlFile(String filePath) throws IOException {
+        StringBuilder sqlQuery = new StringBuilder();
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(filePath), "UTF-8"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                sqlQuery.append(line).append(" ");
+            }
+        }
+
+        return sqlQuery.toString().trim();
+    }
+}
+
+
